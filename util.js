5c91b709f8b5793724813a916602d9b056ef47af
laco1221 <laszlo.hornyak@ennosol.eu>
2015-08-05 10:36:08 +0200

    Add roles multiple places

--- util.js
+++ util.js
@@ -23,7 +23,7 @@ function escapeRegExp (str) {
   return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
 }
 
-function rewrite (args) {
+function rewrite(args) {
   // check if splicable is already in the body text
   var re = new RegExp(args.splicable.map(function (line) {
     return '\s*' + escapeRegExp(line);
@@ -36,26 +36,48 @@ function rewrite (args) {
   var lines = args.haystack.split('\n');
 
   var otherwiseLineIndex = -1;
-  lines.forEach(function (line, i) {
-    if (line.indexOf(args.needle) !== -1) {
-      otherwiseLineIndex = i;
+  if (args.all) {
+    // Adds the code everywhere the needle is found
+    lines.forEach(function (line, i) {
+      if (line.indexOf(args.needle) !== -1) {
+        otherwiseLineIndex = i;
+        var spaces = 0;
+        while (lines[otherwiseLineIndex].charAt(spaces) === ' ') {
+          spaces += 1;
+        }
+
+        var spaceStr = '';
+        while ((spaces -= 1) >= 0) {
+          spaceStr += ' ';
+        }
+
+        lines.splice(otherwiseLineIndex + 1, 0, args.splicable.map(function (line) {
+          return spaceStr + line;
+        }).join('\n'));
+      }
+    });
+  } else {
+    lines.forEach(function (line, i) {
+      if (line.indexOf(args.needle) !== -1) {
+        otherwiseLineIndex = i;
+      }
+    });
+    if (otherwiseLineIndex === -1) return lines.join('\n');
+
+    var spaces = 0;
+    while (lines[otherwiseLineIndex].charAt(spaces) === ' ') {
+      spaces += 1;
     }
-  });
-  if(otherwiseLineIndex === -1) return lines.join('\n');
 
-  var spaces = 0;
-  while (lines[otherwiseLineIndex].charAt(spaces) === ' ') {
-    spaces += 1;
+    var spaceStr = '';
+    while ((spaces -= 1) >= 0) {
+      spaceStr += ' ';
+    }
+
+    lines.splice(otherwiseLineIndex + 1, 0, args.splicable.map(function (line) {
+      return spaceStr + line;
+    }).join('\n'));
   }
-
-  var spaceStr = '';
-  while ((spaces -= 1) >= 0) {
-    spaceStr += ' ';
-  }
-
-  lines.splice(otherwiseLineIndex + 1, 0, args.splicable.map(function (line) {
-    return spaceStr + line;
-  }).join('\n'));
 
   return lines.join('\n');
 }
